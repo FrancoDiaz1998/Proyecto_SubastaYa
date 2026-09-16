b739ea0af73c38c7e87aa6a4380861e6266661e8
Franco Díaz <francogermandiaz.1998@gmail.com>
2026-09-16 14:35:12 -0300

    Franco/backend frontend (#4)
    * Módulos 2-5 Ok
    
    * Fix:property

--- backend/src/SubastaYa.Application/UseCases/Autenticacion/Login/LoginRequest.cs
+++ backend/src/SubastaYa.Application/UseCases/Autenticacion/Login/LoginRequest.cs
@@ -2,6 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SubastaYa.Application.UseCases.Autenticacion.Login;
 
-public sealed record LoginRequest(
-    [property: Required, EmailAddress] string Email, 
-    [property: Required] string Password);
+public sealed class LoginRequest
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; } = string.Empty;
+
+    [Required]
+    public string Password { get; init; } = string.Empty;
+}
